578dfa58e83901e419859b14a7ab8f207b1bb5af
GautamJadhav123 <gautamjadhav12597@gmail.com>
2023-05-20 20:22:16 +0530

    admin table added

--- src/main/java/com/insuranceApp/model/Admin.java
+++ src/main/java/com/insuranceApp/model/Admin.java
@@ -1,10 +1,18 @@
 package com.insuranceApp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table (name = "Admins")
 public class Admin {
 	
 	
-	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer adminId;
 	
 	private String name;
@@ -13,4 +21,39 @@ public class Admin {
 	
 	private String password;
 
+	public Integer getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
 }
